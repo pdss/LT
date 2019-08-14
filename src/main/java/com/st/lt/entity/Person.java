6fdc397f4057719f1b10f790313e3abfe82ea467
luotao <307958201@qq.com>
2019-08-15 00:54:57 +0800

    test

---
+++ src/main/java/com/st/lt/entity/Person.java
@@ -0,0 +1,14 @@
+package com.st.lt.entity;
+
+import lombok.Data;
+
+/**
+ * @Author: luotao
+ * @CreateTime: 2019-08-15 00:53
+ * @Description:
+ */
+@Data
+public class Person {
+    private String id;
+    private String name;
+}
